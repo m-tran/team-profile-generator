2187d1da4fd64b7ee44f667c277e0e3517e8d02a
Michelle Tran <mptran0101@berkeley.edu>
2020-07-04 16:02:59 -0700

    created Engineer class

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,24 +1,24 @@
 // TODO: Write code to define and export the Employee class
-var Employee = function(name, id, email) {
+function Employee(name, id, email) {
     this.name = name;
     this.id = id;
     this.email = email;
 };
 
 Employee.prototype.getName = function() {
-    returns(this.name);
+    return this.name;
 };
 
 Employee.prototype.getId = function() {
-    returns(this.id);
+    return this.id;
 };
 
 Employee.prototype.getEmail = function() {
-    returns(this.email);
+    return this.email;
 };
 
 Employee.prototype.getRole = function() {
-    returns("Employee");
+    return "Employee";
 };
 
 module.exports = Employee; 
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,12 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+function Engineer(officeNumber) {
+    this.officeNumber = officeNumber;
+};
+
+Engineer.prototype.getRole = function() {
+    return "Engineer";
+};
+
+module.exports = Engineer;
