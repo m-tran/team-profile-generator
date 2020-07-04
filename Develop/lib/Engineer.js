56d81087fd1c5c77712912426a8bee6cdb9b48fc
Michelle Tran <mptran0101@berkeley.edu>
2020-07-04 16:05:08 -0700

    modified Engineer class

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,8 +1,12 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 const Employee = require("./Employee");
 
-function Engineer(officeNumber) {
-    this.officeNumber = officeNumber;
+function Engineer(github) {
+    this.github = github;
+};
+
+Engineer.prototype.getGithub = function() {
+    return this.github;
 };
 
 Engineer.prototype.getRole = function() {
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,3 +1,7 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
 const Employee = require("./Employee");
 
+function Intern() {
+    
+};
+
