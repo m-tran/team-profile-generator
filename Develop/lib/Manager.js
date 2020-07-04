b3735f66c05c7ebaaaaaaa0406f45d966a247578
Michelle Tran <mptran0101@berkeley.edu>
2020-07-04 16:09:29 -0700

    created Manager class

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,12 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+function Manager(officeNumber) {
+    this.officeNumber = officeNumber;
+};
+
+Manager.prototype.getRole = function() {
+    return "Manager";
+};
+
+module.exports = Manager;
