7c83ee554f77f23c54d88310727d4bb571324060
Michelle Tran <mptran0101@berkeley.edu>
2020-07-04 16:07:31 -0700

    created Intern class

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,7 +1,16 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
 const Employee = require("./Employee");
 
-function Intern() {
-    
+function Intern(school) {
+    this.school = school;
 };
 
+Intern.prototype.getSchool = function () {
+    return this.school;
+}
+
+Intern.prototype.getRole = function() {
+    return "Intern";
+}
+
+module.exports = Intern;
